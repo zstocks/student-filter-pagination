96411c3e9e3f1f213541f1c74d4ba468b558bada
Zachary Stocks <stocks.zachary@gmail.com>
2021-08-20 17:36:33 -0600

    Added search bar
    Added a search bar and filter functionality to the page.

--- js/script.js
+++ js/script.js
@@ -3,82 +3,80 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */ 
 
+// create and insert search form onto page
+const searchFormHTML = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`;
+document.querySelector('.header').insertAdjacentHTML('beforeend', searchFormHTML);
 
+//create and insert new div used to display a 'no results' message to the user if their search yields no results
+const noResults = document.createElement('div');
+noResults.className = 'no-results';
+document.querySelector('.page').append(noResults);
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+// select DOM elements that will be used throughout the app
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
+const searchInput = document.querySelector('#search');
+const searchBtn = document.querySelector('.student-search button');
 
 
 
 /**
- * Creates and inserts elements needed to display a "page" of nine students
+ * creates and inserts elements needed to display a "page" of nine students
  * 
  * @param {array} list - an array containing student data to be accessed and displayed
  * @param {number} page - the page number corresponding to the pagination button that was clicked (1 on page load)
  */
 const showPage = (list, page) => {
-   // set variables to create  a range of nine indexes to be shown on the page based on the page number clicked
+   // set variables to create a range of nine indexes corresponding to the students to be shown on the page being generated
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector('.student-list');
-
    //Clear any students from student-list
    studentList.innerHTML = '';
-
-   // loop through the list element
+   // generate HTML for the list items and insert them into the page
    for (let i = 0; i < list.length; i++) {
-
-      // generate HTML for the list items to be shown on the page 
       if (i >= startIndex && i < endIndex) {
          const li = `
          <li class="student-item cf">
            <div class="student-details">
-             <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
-             <h3>${data[i].name.first + ' ' + data[i].name.last}</h3>
-             <span class="email">${data[i].email}</span>
+             <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+             <h3>${list[i].name.first + ' ' + list[i].name.last}</h3>
+             <span class="email">${list[i].email}</span>
            </div>
            <div class="joined-details">
-             <span class="date">Joined ${data[i].registered.date}</span>
+             <span class="date">Joined ${list[i].registered.date}</span>
            </div>
          </li>`;
-
-         // insert the HTML into the student-list ul element
          studentList.insertAdjacentHTML('beforeend', li);
       }
    }
 }
 
 /**
- * Creates and inserts elements needed to display the pagination buttons
+ * creates and inserts elements needed to display the pagination buttons
  * 
  * @param (array) list - an array containing student data
  */
 const addPagination = list => {
-   // get number of needed pages
+   // get number of needed pages and clear out the link-list ul element
    const pages = list.length / 9;
-   const linkList = document.querySelector('.link-list');
-
-   // clear any previous data from link-list
    linkList.innerHTML = '';
-
-   //loop over pages to generate HTML for the number of needed buttons
+   // generate HTML for the buttons and insert them into the page
    for (let i = 0; i < pages; i++) {
       const li = `
       <li>
          <button type="button">${i + 1}</button>
       </li>`;
-
-      // insert HTML into the link-list ul element
       linkList.insertAdjacentHTML('beforeend', li);
    }
-
    // give the first pagination button the class 'active'
    const paginationButtons = document.querySelectorAll('.link-list li > button');
    paginationButtons[0].className = 'active';
-
+   // when a button is clicked set 'active' class to clicked button and display students for that page
    linkList.addEventListener('click', (e) => {
       const btnClicked = e.target;
       if (btnClicked.tagName === 'BUTTON') {
@@ -91,8 +89,37 @@ const addPagination = list => {
    });
 }
 
+/**
+ * filters students by comparing student names to search input
+ */
+const filterResults = () => {
+   let newList = [];
+   const searchTerm = searchInput.value.toUpperCase();
+   // add students with names matching search input to the newList array
+   for (let i = 0; i < data.length; i++) {
+      const name = (data[i].name.first + ' ' + data[i].name.last).toUpperCase();
+      if (name.includes(searchTerm)) {
+         newList.push(data[i]);
+      }
+   }
+   // clear any text out of the no-results block that may exist from previous search
+   noResults.innerHTML = '';
+   // if search returns no result, inform the user - otherwise, display filtered results
+   if (newList.length < 1) {
+      const noResultsMsg = `<p>Sorry, your search did not match any results.</p>`;
+      noResults.insertAdjacentHTML('beforeend', noResultsMsg);
+      studentList.innerHTML = '';
+      linkList.innerHTML = '';
+   } else {
+      showPage(newList, 1);
+      addPagination(newList);
+   }
+}
 
-
-// Call functions
+// display students and pagination on the page
 showPage(data, 1);
 addPagination(data);
+
+// filter results when user performs a search
+searchInput.addEventListener('keyup', filterResults);
+searchBtn.addEventListener('click', filterResults);
